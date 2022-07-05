8a4f172956d79d9651bcc0149568ee1f9b5450b2
SunGod7 <sun7ruler@gmail.com>
2022-07-05 08:19:40 -0400

    struggled with this one

--- server.js
+++ server.js
@@ -4,25 +4,60 @@ const app = express()
 
 const port = 3000
 
+//home page
 
 app.get('/', (req, res) => {
     
-    res.send('99 Bottles of beer on the wall')
-})
+        res.send(`<h1>99 Bottles of beer on the wall</h1> <a href="/number_of_bottles/">take one down, pass it around</a>`)
+    })
+
+    // console.log()
 
 
 
-app.get('/:i/:i/:brews', (req, res) => {
+//index
+//show
+app.get('/number_of_bottles', (req, res) => {
     
-    console.log(req.params.i)
-    for (let i = 100; i > 0; i--) {
-        let brews = (i - 1) 
+    // const number_of_bottles = parseInt(req.params.number_of_bottles)
+	// const product = number_of_bottles - 1
+   
+   //let number_of_bottles = 98
+    
+    //   if(number_of_bottlesId == number_of_bottles) {
+    //      --1
+    //   }
+     
+    res.send('<a href="/brews/">bottles of beer on the wall</a>')
+    
+    
+    //  console.log(req.params.i)
+    // for (let i = 100; i > 0; i--) {
+    //      let number_of_bottles = (i - 1) 
          
-    }  
-    res.send(` ${req.params.i}  bottles of beer on the wall,${req.params.i}  bottles of beer. 
-    Take one down and pass it around ${req.params.brews}  bottles of beer on the wall.`)
+       
+    //      res.send(` ${req.params.number_of_bottles}  bottles of beer on the wall, ${req.params.number_of_bottles}  bottles of beer. 
+    //      Take one down and pass it around ${req.params.number_of_bottles}  bottles of beer on the wall.`)
+    // }
+
 })
 
+
+
+
+
+
+
+
+
+
+// app.get('/brews', (req, res) => {
+//         if(brews == number_of_bottles) {
+//             let brews = number_of_bottles - 1
+//         }
+//         console.log(req.params.brews)
+//         res.render(req.params.brews)
+//     })
 
 
 
